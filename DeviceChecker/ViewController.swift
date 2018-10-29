1c09c34870d8ae712f95e49968a2bec0cc944afc
Axel <soulchildsince2013@gmail.com>
2018-10-29 16:42:24 +0800

    Initial Commit

---
+++ DeviceChecker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DeviceChecker
+//
+//  Created by Soulchild on 29/10/2018.
+//  Copyright © 2018 fluffy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
